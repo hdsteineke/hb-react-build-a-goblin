ebef947343142a50f67f85c40d5db33a75bda89b
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-05 09:52:20 -0700

    is the test working now

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/HP/i);
+  const linkElement = screen.getByText(/Filter Goblins/i);
   expect(linkElement).toBeInTheDocument();
 });
